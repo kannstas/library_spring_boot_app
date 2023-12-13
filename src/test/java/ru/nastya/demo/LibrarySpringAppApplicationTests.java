47dfa9414e826f5416e6fc2a39b265fcfdac73e3
kannstas <k.annstas@gmail.com>
2023-12-13 18:10:24 +0300

    Initial commit

---
+++ src/test/java/ru/nastya/demo/LibrarySpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nastya.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
